1b0fcfb6fe8756a2140fee8effad3b00d301bba6
David Cruz <dacruz04@optonline.net>
2020-06-08 22:04:38 -0400

    uh

---
+++ src/apcsa/Board.java
@@ -0,0 +1,10 @@
+package apcsa;
+
+public class Board {
+	// slots[x coordinate starting from bottom left][y coordinate starting from bottom left]
+	private Slot[][] Slots = new Slot[6][7];
+	Board() {
+		
+	}
+
+}
