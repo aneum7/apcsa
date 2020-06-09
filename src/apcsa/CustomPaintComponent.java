a4548873f1e47b7445710049eb11be466e6048f3
Andrew <aneum7@yandex.com>
2020-06-09 01:45:34 -0400

    make grid have slots instead of just squares

--- src/apcsa/CustomPaintComponent.java
+++ src/apcsa/CustomPaintComponent.java
@@ -10,13 +10,13 @@ public class CustomPaintComponent extends Component {
 		Graphics2D g2d = (Graphics2D)g;
 		// here we paint
 		
-		g2d.setColor(Color.black);
-		g2d.fillRect(0, 0, 700, 600);
 		g2d.setColor(Color.WHITE);
+		g2d.fillRect(0, 0, 700, 600);
+		g2d.setColor(Color.black);
 		
 		for (int i = 0; i < 7; i++) {
 			for (int k = 0; k < 6; k++) {
-				g2d.drawRect(i * 100, k * 100, 100, 100);
+				g2d.fillOval(i * 100, k * 100, 100, 100);
 			}
 		}
 	}
