04cd2729a6d1079d9b80d6a4d5ba2f64eb279799
Andrew <aneum7@yandex.com>
2020-06-09 05:48:59 -0400

    update checkLeft

--- src/apcsa/Board.java
+++ src/apcsa/Board.java
@@ -14,9 +14,10 @@ public class Board {
 	
 
 	/**
-	 * looks throughout the board for a win (4 in a row)
-	 * returns 2 for blue win
-	 * returns 1 for red win returns 0 for no win
+	 * the following win condition methods:
+	 * return 4 if red has won
+	 * return 20 if blue has won
+	 * return other if no player has yet won
 	 */
 	public int checkForWin() {
 		for (int i = 0; i < 7; i++) {
@@ -30,26 +31,12 @@ public class Board {
 		return 0;
 	}
 
-	/**
-	 * looks to the left for a win (3 additional)
-	 * returns 2 for blue win
-	 * returns 1 for red win returns 0 for no win
-	 */
-	public int checkForLeftWin(int i, int j) {
-		int count = 0;
-		try {
-			for (int t = 1; t < 4; t++) {
-				if (Slots[j][i] == Slots[j - t][i]) {
-					count++;
-				}
-			}
-		} catch (Exception e) {
-			return 0;
-		}
-		if (count == 3) {
-			return Slots[j][i];
-		} else {
-			return 0;
+	public int checkLeft (int i, int j) {
+		if (i < 3) return 0;
+		else {
+			int sum = 0;
+			for (int k = 0; k < 4; k++) sum += getSlot(i - k, j);
+			return sum;
 		}
 	}
 
